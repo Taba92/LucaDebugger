e3f7d7f7567db1546b98f70005d1d6846a200384
Luca <luca@Luca.homenet.telecomitalia.it>
2020-08-08 19:24:24 +0200

    erased beam files

--- lucaCase-studies/case3/watcher.erl
+++ lucaCase-studies/case3/watcher.erl
@@ -2,13 +2,12 @@
 -export([main/0,child/1]).
 
 main()->
+	process_flag(trap_exit,true),
 	spawn_link(?MODULE,child,[true]),
 	spawn_link(?MODULE,child,[false]),
-        process_flag(trap_exit,true),
-        startSupervision().
 	%spawn_link(?MODULE,child,[false]),
 	%spawn_link(?MODULE,child,[true]),
-	
+	startSupervision().
 
 child(Flag)->
 	process_flag(trap_exit,Flag),

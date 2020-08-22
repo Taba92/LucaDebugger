8af31d4d6596cf70a28ff46f565cdc1572648bd9
ivan <ivan.lanese@gmail.com>
2020-08-23 00:58:34 +0200

    Prova

--- lucaCase-studies/case3/watcher.erl
+++ lucaCase-studies/case3/watcher.erl
@@ -1,16 +1,18 @@
 -module(watcher).
--export([main/0,child/1]).
+-export([main/0,child/2]).
 
 main()->
-	spawn_link(?MODULE,child,[true]),
-	spawn_link(?MODULE,child,[false]),
+	spawn_link(?MODULE,child,[true,self()]),
+	spawn_link(?MODULE,child,[false,self()]),
 	process_flag(trap_exit,true),
 	%spawn_link(?MODULE,child,[false]),
 	%spawn_link(?MODULE,child,[true]),
+        receive X -> X end,
 	startSupervision().
 
-child(Flag)->
+child(Flag,Pid)->
 	process_flag(trap_exit,Flag),
+        Pid!Pid,
 	3/1,
 	self().
 
